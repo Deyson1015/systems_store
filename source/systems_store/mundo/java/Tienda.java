db0cf648d9dceeb1cc4122ca553a5c4e80a94eb6
Deyson Urrego <deysonurrego159@gmail.com>
2025-06-18 22:16:02 -0500

    Avances al proyecto

--- source/systems_store/mundo/java/Tienda.java
+++ source/systems_store/mundo/java/Tienda.java
@@ -1,5 +1,31 @@
 package systems_store.mundo.java;
 
-public class Tienda {
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import systems_store.mundo.java.Producto;
 
+
+public class Tienda {
+	
+	// Ordenar los productos por precio de menor a mayor
+	public static void ordenarPorPrecio(List<Producto> productos) {
+		Collections.sort(productos, Comparator.comparingDouble(Producto::getPrecio));
+	}
+	
+	// Ordenar por nombre alfabeticamente
+	public static void ordenarPorNombre(List<Producto> productos) {
+		Collections.sort(productos, Comparator.comparing(Producto::getNombre));
+	}
+	
+	// Ordenar por cantidad de menor a mayor
+	public static void ordenarPorCantidad(List<Producto> productos) {
+		Collections.sort(productos, Comparator.comparingInt(Producto::getCantidad));
+	}
+
+	// Ordenar por fecha de ingreso del producto
+	public static void ordenarPorFecha(List<Producto> productos) {
+		Collections.sort(productos, Comparator.comparing(Producto::getFechaIngreso));
+	}
+	
 }
